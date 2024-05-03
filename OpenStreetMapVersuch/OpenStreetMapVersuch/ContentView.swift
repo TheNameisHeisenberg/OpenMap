aa0e0c88866913d833b22a9bb2391d67ea86aa52
TheNameisHeisenberg <136227951+TheNameisHeisenberg@users.noreply.github.com>
2024-05-03 15:58:01 +0200

    Add files via upload

--- OpenStreetMapVersuch/OpenStreetMapVersuch/ContentView.swift
+++ OpenStreetMapVersuch/OpenStreetMapVersuch/ContentView.swift
@@ -6,34 +6,46 @@
 //
 
 import SwiftUI
-
-import SwiftUI
-
 struct ContentView: View {
     @State private var city: String = ""
-    @State private var restaurants: [String] = []
-    @State private var museums: [String] = []
-    @State private var parks: [String] = []
+    @State private var restaurants: [POI] = []
+    @State private var museums: [POI] = []
+    @State private var parks: [POI] = []
     
     var body: some View {
         VStack {
             SearchBar(city: $city, restaurants: $restaurants, museums: $museums, parks: $parks)
             
             Section(header: Text("Restaurants")) {
-                List(restaurants, id: \.self) { restaurant in
-                    Text(restaurant)
+                List(restaurants, id: \.id) { restaurant in
+                    VStack(alignment: .leading) {
+                        Text("Name: \(restaurant.name)")
+                        Text("Location: \(restaurant.location)")
+                        Text("Rating: \(restaurant.rating)")
+                        Text("Category: \(restaurant.category.rawValue)")
+                    }
                 }
             }
             
             Section(header: Text("Museums")) {
-                List(museums, id: \.self) { museum in
-                    Text(museum)
+                List(museums, id: \.id) { museum in
+                    VStack(alignment: .leading) {
+                        Text("Name: \(museum.name)")
+                        Text("Location: \(museum.location)")
+                        Text("Rating: \(museum.rating)")
+                        Text("Category: \(museum.category.rawValue)")
+                    }
                 }
             }
             
             Section(header: Text("Parks")) {
-                List(parks, id: \.self) { park in
-                    Text(park)
+                List(parks, id: \.id) { park in
+                    VStack(alignment: .leading) {
+                        Text("Name: \(park.name)")
+                        Text("Location: \(park.location)")
+                        Text("Rating: \(park.rating)")
+                        Text("Category: \(park.category.rawValue)")
+                    }
                 }
             }
         }
--- OpenStreetMapVersuch/OpenStreetMapVersuch/SearchBar.swift
+++ OpenStreetMapVersuch/OpenStreetMapVersuch/SearchBar.swift
@@ -15,9 +15,9 @@ import OSLog
 
 struct SearchBar: View {
     @Binding var city: String
-    @Binding var restaurants: [String]
-        @Binding var museums: [String]
-        @Binding var parks: [String]
+    @Binding var restaurants: [POI]
+        @Binding var museums: [POI]
+        @Binding var parks: [POI]
         private let logger = Logger()
     
     var body: some View {
@@ -32,20 +32,23 @@ struct SearchBar: View {
                         Text("Search")
                     }
                 }
+                .padding()
+                
                 List(restaurants, id: \.self) { restaurant in
-                    Text(restaurant)
+                    Text(restaurant.name)
                 }
                 .padding(.top)
+                
                 List(museums, id: \.self) { museum in
-                    Text(museum)
+                    Text(museum.name)
                 }
                 .padding(.top)
+                
                 List(parks, id: \.self) { park in
-                    Text(park)
+                    Text(park.name)
                 }
                 .padding(.top)
             }
-            .padding()
         }
     
     private func fetchPlaces(for city: String) {
@@ -59,11 +62,15 @@ struct SearchBar: View {
             
             // Fetch parks
             fetchParks(for: city)
+        
+            // After fetching all data, save it to a CSV file
+            saveDataToCSV(data: combineData(), filename: "places.csv")
         }
     
     
     private func fetchRestaurants(for city: String) {
         logger.log("Fetching Started")
+        
         guard let url = URL(string: "http://overpass-api.de/api/interpreter?data=[out:json];area[name=\"\(city)\"]->.searchArea;node[amenity=restaurant](area.searchArea);out 10;")
         else {
             logger.log("Invalid URL")
@@ -87,17 +94,29 @@ struct SearchBar: View {
                 let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
                 logger.log("Fetching completed")
                 if let elements = json?["elements"] as? [[String: Any]] {
-                    self.restaurants = elements.compactMap { $0["tags"] as? [String: String] } // Extract tags
-                                                .compactMap { $0["name"] } // Get restaurant name
+                    var fetchedRestaurants: [POI] = []
+                    for element in elements {
+                        if let tags = element["tags"] as? [String: String],
+                           let name = tags["name"],
+                           let latitude = element["lat"] as? Double,
+                           let longitude = element["lon"] as? Double {
+                            let location = "(\(latitude), \(longitude))"
+                            let rating = Double(tags["rating"] ?? "0") ?? 0.0
+                            fetchedRestaurants.append(POI(name: name, location: location, rating: rating, category: .FoodAndDrink))
+                        }
+                    }
+                    self.restaurants = fetchedRestaurants
                 }
             } catch {
                 logger.log("Error parsing JSON: \(error.localizedDescription)")
             }
         }.resume()
     }
+
     
     private func fetchParks(for city: String) {
         logger.log("Fetching Parks Started")
+        
         guard let url = URL(string: "http://overpass-api.de/api/interpreter?data=[out:json];area[name=\"\(city)\"]->.searchArea;node[leisure=park](area.searchArea);out 10;")
         else {
             logger.log("Invalid URL")
@@ -121,17 +140,29 @@ struct SearchBar: View {
                 let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
                 logger.log("Fetching Parks completed")
                 if let elements = json?["elements"] as? [[String: Any]] {
-                    self.parks = elements.compactMap { $0["tags"] as? [String: String] } // Extract tags
-                                                .compactMap { $0["name"] } // Get park name
+                    var fetchedParks: [POI] = []
+                    for element in elements {
+                        if let tags = element["tags"] as? [String: String],
+                           let name = tags["name"],
+                           let latitude = element["lat"] as? Double,
+                           let longitude = element["lon"] as? Double {
+                            let location = "(\(latitude), \(longitude))"
+                            let rating = Double(tags["rating"] ?? "0") ?? 0.0
+                            fetchedParks.append(POI(name: name, location: location, rating: rating, category: .Nature))
+                        }
+                    }
+                    self.parks = fetchedParks
                 }
             } catch {
                 logger.log("Error parsing JSON: \(error.localizedDescription)")
             }
         }.resume()
     }
+
     
     private func fetchMuseums(for city: String) {
         logger.log("Fetching Museums Started")
+        
         guard let url = URL(string: "http://overpass-api.de/api/interpreter?data=[out:json];area[name=\"\(city)\"]->.searchArea;node[amenity=museum](area.searchArea);out 10;")
         else {
             logger.log("Invalid URL")
@@ -146,7 +177,7 @@ struct SearchBar: View {
             
             // Log JSON data
             if let jsonString = String(data: data, encoding: .utf8) {
-                self.logger.log("Museums JSON Data: \(jsonString)")
+                self.logger.log("JSON Data: \(jsonString)")
             } else {
                 self.logger.log("Failed to convert data to string")
             }
@@ -155,12 +186,63 @@ struct SearchBar: View {
                 let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
                 logger.log("Fetching Museums completed")
                 if let elements = json?["elements"] as? [[String: Any]] {
-                    self.museums = elements.compactMap { $0["tags"] as? [String: String] } // Extract tags
-                                                .compactMap { $0["name"] } // Get museum name
+                    var fetchedMuseums: [POI] = []
+                    for element in elements {
+                        if let tags = element["tags"] as? [String: String],
+                           let name = tags["name"],
+                           let latitude = element["lat"] as? Double,
+                           let longitude = element["lon"] as? Double {
+                            let location = "(\(latitude), \(longitude))"
+                            let rating = Double(tags["rating"] ?? "0") ?? 0.0
+                            fetchedMuseums.append(POI(name: name, location: location, rating: rating, category: .ArtAndCulture))
+                        }
+                    }
+                    self.museums = fetchedMuseums
                 }
             } catch {
                 logger.log("Error parsing JSON: \(error.localizedDescription)")
             }
         }.resume()
     }
+
+    
+    private func combineData() -> [[String]] {
+        var combinedData: [[String]] = []
+        
+        // Add restaurants data
+        for restaurant in restaurants {
+            combinedData.append(["\(restaurant.location)", restaurant.name, "\(restaurant.rating)"])
+        }
+        
+        // Add museums data
+        for museum in museums {
+            combinedData.append(["\(museum.location)", museum.name, "\(museum.rating)"])
+        }
+        
+        // Add parks data
+        for park in parks {
+            combinedData.append(["\(park.location)", park.name, "\(park.rating)"])
+        }
+        
+        return combinedData
+    }
+    
+    private func saveDataToCSV(data: [[String]], filename: String) {
+        let fileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent(filename)
+        
+        do {
+            var csvText = "poiID,location(x,y),title,rating\n"
+            var poiID = 0
+            for row in data {
+                let rowText = "\(poiID),\(row[0]),\(row[1]),\(row[2])\n"
+                csvText.append(rowText)
+                poiID += 1
+            }
+            try csvText.write(to: fileURL, atomically: true, encoding: .utf8)
+            logger.log("Data saved to CSV file: \(filename)")
+        } catch {
+            logger.log("Error saving data to CSV file: \(error.localizedDescription)")
+        }
+    }
+
 }
